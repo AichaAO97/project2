3ed2f31bea41d9ed6611e7b469e5e67aa3fe2715
AichaAO97 <aicha.ait.oumghar17@gmail.com>
2023-03-17 09:37:58 +0100

    add localStorage and fix the cursors and the editing buttons & the flex-container to make the page responsive but the tasks are not draggable on mobile devices

--- js/script1.js
+++ js/script1.js
@@ -1,5 +1,6 @@
 let data_value = 0;
 
+/// check wether we use a mobile or a laptop
 const checkTouchDevice = () => {
   try {
     //We try to create TouchEvent (it would fail for desktops and throw error)
@@ -9,6 +10,19 @@ const checkTouchDevice = () => {
     return false;
   }
 };
+
+let currentElement;
+
+//Drag and drop functions
+function dragStart(e) {
+  initialX = checkTouchDevice() ? e.touches[0].clientX : e.clientX;
+  initialY = checkTouchDevice() ? e.touches[0].clientY : e.clientY;
+  //Set current Element
+  currentElement = e.target;
+}
+function dragOver(e) {
+  e.preventDefault();
+}
 
 //Returns element index with given value
 const getPosition = (value) => {
@@ -23,27 +37,6 @@ const getPosition = (value) => {
   return elementIndex;
 };
 
-// Get all the columns
-
-let currentElement;
-//Drag and drop functions
-function dragStart(e) {
-  initialX = checkTouchDevice() ? e.touches[0].clientX : e.clientX;
-  initialY = checkTouchDevice() ? e.touches[0].clientY : e.clientY;
-  //Set current Element
-  currentElement = e.target;
-}
-function dragOver(e) {
-  e.preventDefault();
-  // let newX = checkTouchDevice() ? e.touches[0].clientX : e.clientX;
-  // let newY = checkTouchDevice() ? e.touches[0].clientY : e.clientY;
-
-  // console.log('x     ', newX);
-  // console.log('y    ', newY);
-  // let targetElement = document.elementFromPoint(newX, newY);
-  // console.log('target  Column  ', targetElement);
-}
-
 const drop = (e) => {
   e.preventDefault();
   // let targetEl = e.target;
@@ -57,7 +50,7 @@ const drop = (e) => {
   let currentValue = currentElement.getAttribute('data-value');
   let targetValue = targetElement.getAttribute('data-value');
 
-  // console.log('targetValue   on drop ......', targetValue);
+  let parentColumn = targetElement.parentNode;
 
   //get index of current and target based on value
   let [currentPosition, targetPosition] = [
@@ -67,17 +60,18 @@ const drop = (e) => {
   initialX = newX;
   initialY = newY;
 
-  // console.log('currentValue:   ', currentValue);
-  // console.log('targetValue:   ', targetValue);
-
   try {
     //'afterend' inserts the element after the target element and 'beforebegin' inserts before the target element
     if (currentPosition < targetPosition) {
       targetElement.insertAdjacentElement('afterend', currentElement);
+      // parentColumn.insertBefore(targetElement, currentElement);
     } else {
       targetElement.insertAdjacentElement('beforebegin', currentElement);
+      // parentColumn.insertBefore(currentElement, targetElement);
     }
   } catch (err) {}
+
+  addToLocalStorage();
 };
 
 // Add drag event listeners to all the divs inside the columns
@@ -102,110 +96,146 @@ function makeDraggabale(task) {
   task.addEventListener('drop', drop);
 }
 
+//load Tasks
+function loadTasks() {
+  // Retrieve the div content from local storage
+  const gotObjectsJSON = localStorage.getItem('objectsJSON');
+  // Convert the JSON string back to an object
+  const gotObjects = JSON.parse(gotObjectsJSON);
+
+  // Get the content from the object and add it to the page
+  for (let i = 1; i <= 3; i++) {
+    let columnParent = document.getElementById(i);
+    let button = columnParent.querySelector('button');
+
+    gotObjects[i.toString()].forEach((obj) => {
+      let arrayOfClasses = Object.values(obj.taskClassList);
+      if (!arrayOfClasses.includes('empty')) {
+        // let emptyDiv = document.createElement('div');
+        // emptyDiv.className = 'task empty';
+        // columnParent.insertBefore(emptyDiv, button);
+        textContent = obj.paragraph;
+        createTask(columnParent, textContent, button);
+      }
+    });
+  }
+}
+
+let objects = {};
+let objectsJSON;
+
+function addToLocalStorage() {
+  objects[1] = [];
+  objects[2] = [];
+  objects[3] = [];
+  let taskItems = document.getElementsByClassName('task');
+  Array.from(taskItems).forEach((taskItem) => {
+    // let paragraph = taskItem.querySelector('p');
+    let taskClassList = taskItem.classList;
+    let paragraph = taskItem.innerText;
+    let ParentColumn = taskItem.parentNode;
+    let ParentColumnId = taskItem.parentNode.id;
+
+    // Create an object to store the div content
+    let obj = { taskClassList: taskClassList, paragraph: paragraph };
+
+    objects[ParentColumnId].push(obj);
+  });
+
+  // const divContent = divElement.innerHTML;
+
+  // Convert the object to a JSON string
+  objectsJSON = JSON.stringify(objects);
+
+  // Store the JSON string in local storage
+  localStorage.setItem('objectsJSON', objectsJSON);
+}
+
+function createTask(column, text, button) {
+  let newTask = document.createElement('div');
+  newTask.className = 'task';
+  newTask.setAttribute('data-value', data_value++);
+  let contentText = document.createElement('p');
+  contentText.className = 'content-task';
+  const textNode = document.createTextNode(text);
+  contentText.appendChild(textNode); // Add the text node to the new <p> element
+  newTask.appendChild(contentText);
+
+  contentText.addEventListener('blur', () => {
+    contentText.contentEditable = false;
+    console.log('Nooo focus');
+  });
+
+  newTask.insertAdjacentHTML(
+    'beforeend',
+    '<ion-icon class="edit-icon" name="create"></ion-icon> <ion-icon class="delete-icon" name="trash"></ion-icon>'
+  );
+
+  let editButton = newTask.querySelector('.edit-icon');
+  editButton.addEventListener('click', function () {
+    editFunction(editButton);
+  });
+  let deleteButton = newTask.querySelector('.delete-icon');
+  deleteButton.addEventListener('click', function () {
+    deleteFunction(deleteButton);
+  });
+  // Append the new task div to the parent column
+  makeDraggabale(newTask);
+
+  column.insertBefore(newTask, button);
+}
+
 addButtons.forEach(function (button) {
   button.addEventListener('click', function () {
     // Get the parent column
     let column = button.parentNode;
 
-    // Create a new task div
-    let newTask = document.createElement('div');
-    newTask.className = 'task';
-    newTask.setAttribute('data-value', data_value++);
-    let contentText = document.createElement('p');
-    contentText.className = 'content-task';
-    contentText.contentEditable = true;
+    // const textNode = document.createTextNode(); // Create a text node
+    createTask(column, 'click to edit!', button);
 
-    const textNode = document.createTextNode('This is some new content!'); // Create a text node
-    contentText.appendChild(textNode); // Add the text node to the new <p> element
-
-    // contentText.addEventListener('focus', function () {
-    //   newTask.draggable = false;
-    // });
-
-    // // Re-enable draggable when editing is complete
-    // contentText.addEventListener('blur', function () {
-    //   newTask.draggable = true;
-    // });
-
-    newTask.appendChild(contentText);
-    newTask.insertAdjacentHTML(
-      'beforeend',
-      '<ion-icon class="edit-icon" name="create"></ion-icon> <ion-icon class="delete-icon" name="trash"></ion-icon>'
-    );
-
-    let editButton = newTask.querySelector('.edit-icon');
-    editButton.addEventListener('click', function (event) {
-      // let clickedIcon = event.target;
-      let editIcon = event.target;
-      console.log(editIcon);
-      let targetDiv = editIcon.parentNode;
-      let targetTask = targetDiv.querySelector('.content-task');
-      console.log(targetTask);
-      targetTask.contentEditable = true;
-    });
-    let deleteButton = newTask.querySelector('.delete-icon');
-    deleteButton.addEventListener('click', function (event) {
-      let deleteIcon = event.target;
-      let targetDiv = deleteIcon.parentNode;
-      let targetColumn = targetDiv.parentNode;
-      targetColumn.removeChild(targetDiv);
-    });
-    // Append the new task div to the parent column
-    makeDraggabale(newTask);
-
-    column.insertBefore(newTask, button);
+    addToLocalStorage();
   });
 });
 
 ////////////////////////////////////// Editing icon /////////////////////
 
+function editFunction(editButton) {
+  let targetDiv = editButton.parentNode;
+  let targetTask = targetDiv.querySelector('.content-task');
+  targetTask.contentEditable = true;
+  targetTask.focus();
+}
+
 let editIcons = document.getElementsByClassName('edit-icon');
-console.log(editIcons);
 Array.from(editIcons).forEach(function (editIcon) {
   editIcon.addEventListener('click', function () {
-    // let clickedIcon = event.target;
-    console.log(editIcon);
-    let targetDiv = editIcon.parentNode;
-    let targetTask = targetDiv.querySelector('.content-task');
-    targetTask.contentEditable = true;
+    editFunction(editButton);
   });
 });
 
 ////////////////////////////////////// Deleting icon /////////////////////
 
+function deleteFunction(deleteButton) {
+  let targetDiv = deleteButton.parentNode;
+  let targetColumn = targetDiv.parentNode;
+  targetColumn.removeChild(targetDiv);
+  addToLocalStorage();
+}
+
 let deleteIcons = document.getElementsByClassName('delete-icon');
-console.log(deleteIcons);
 
 Array.from(deleteIcons).forEach(function (deleteIcon) {
   deleteIcon.addEventListener('click', function () {
-    let targetDiv = deleteIcon.parentNode;
-    let targetColumn = targetDiv.parentNode;
-    targetColumn.removeChild(targetDiv);
+    deleteFunction(deleteIcon);
   });
 });
 
-/*
+///////////////////////////////////////////////////////////////////
 
-function drop(event) {
-  event.preventDefault();
-  var targetDiv = event.target;
+window.addEventListener('beforeunload', function () {
+  addToLocalStorage();
+});
 
-  // Make sure we're not dropping onto the same div or onto a button
-  if (draggedDiv !== targetDiv && !targetDiv.matches('button')) {
-    // Get the parent columns of the dragged and target divs
-    var draggedColumn = draggedDiv.parentNode;
-    var targetColumn = targetDiv.parentNode;
-
-    // Move the dragged div to the target column
-    targetColumn.appendChild(draggedDiv);
-
-    // If the dragged column is now empty, add a placeholder div
-    if (draggedColumn.childElementCount === 0) {
-      var placeholderDiv = document.createElement('div');
-      placeholderDiv.classList.add('placeholder');
-      draggedColumn.appendChild(placeholderDiv);
-    }
-  }
-}
-
-*/
+window.onload = async () => {
+  loadTasks();
+};
